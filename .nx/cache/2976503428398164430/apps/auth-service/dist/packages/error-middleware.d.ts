c2a159d5437fa1c8d81f6c321b923f819c2ffe72
Vishal Garg <vgarg@net32.com>
2025-06-01 02:21:25 +0530

    db setup

---
+++ .nx/cache/2976503428398164430/apps/auth-service/dist/packages/error-middleware.d.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from "express";
+export declare const errorMiddleware: (err: Error, req: Request, res: Response) => Response<any, Record<string, any>>;
+//# sourceMappingURL=error-middleware.d.ts.map
---
+++ apps/auth-service/dist/packages/error-middleware.js
@@ -0,0 +1,44 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var error_middleware_exports = {};
+__export(error_middleware_exports, {
+  errorMiddleware: () => errorMiddleware
+});
+module.exports = __toCommonJS(error_middleware_exports);
+var import__ = require(".");
+const errorMiddleware = (err, req, res) => {
+  if (err instanceof import__.AppError) {
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+      details: err.details
+    });
+  }
+  console.error("Unhandled Error", err);
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+    details: "An unexpected error occurred. Please try again later."
+  });
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  errorMiddleware
+});
+//# sourceMappingURL=error-middleware.js.map
