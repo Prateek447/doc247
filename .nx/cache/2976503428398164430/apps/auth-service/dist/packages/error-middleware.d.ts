a75a490fa9a98f9d9706b57ca06af4c045ddae20
Vishal Garg <vgarg@net32.com>
2025-06-07 01:02:17 +0530

    nx to gitignore

--- .nx/cache/2976503428398164430/apps/auth-service/dist/packages/error-middleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from "express";
-export declare const errorMiddleware: (err: Error, req: Request, res: Response) => Response<any, Record<string, any>>;
-//# sourceMappingURL=error-middleware.d.ts.map
